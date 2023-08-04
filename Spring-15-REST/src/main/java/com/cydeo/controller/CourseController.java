e7b265341406b183fc86de202ee0b70631c73611
raduga9199 <raduga9199@gmail.com>
2023-08-04 10:25:46 -0400

    getCourseById

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -2,15 +2,14 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@RestController
+@RestController //@Controller + @ResponseBody
 @RequestMapping("/courses")
 public class CourseController {
     private final CourseService courseService;
@@ -29,6 +28,10 @@ public class CourseController {
 
     }
 
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id") Long courseId) {
+        return courseService.getCourseById(courseId);
+    }
 
 
 }
