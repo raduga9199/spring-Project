3a5f3dd915c8012120288ab6cb7dde3d23077136
raduga9199 <raduga9199@gmail.com>
2023-08-04 11:10:48 -0400

    /api/v2

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -2,15 +2,12 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController //@Controller + @ResponseBody
-@RequestMapping("/courses")
+@RequestMapping("/courses/api/v1")
 public class CourseController {
     private final CourseService courseService;
 
@@ -36,6 +33,26 @@ public class CourseController {
     @GetMapping("category/{name}")
     public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
         return courseService.getCoursesByCategory(category);
+    }
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
+    }
+
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO course){
+         courseService.updateCourse(courseId,course);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourse(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+    }
+
+    @DeleteMapping
+    public void deleteCourse(){
+        courseService.deleteCourses();
     }
 
 
