4aff870519a6f7cf3129cbdf3cab3d4223bc8f21
raduga9199 <raduga9199@gmail.com>
2023-08-07 07:40:31 -0400

    RestConsuming

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -1,7 +1,9 @@
-package com.cydeo.spring17restconsumingapis;
+package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring17RestConsumingApisApplication {
@@ -10,4 +12,8 @@ public class Spring17RestConsumingApisApplication {
 		SpringApplication.run(Spring17RestConsumingApisApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
